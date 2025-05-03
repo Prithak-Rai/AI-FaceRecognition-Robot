3085915d2df14fb2d2f4209addb7b3bc0190d756
Prithak Rai <prithak.khamtu@gmail.com>
2025-05-03 15:14:12 +0545

    Delete mobile_app directory

--- mobile_app/android/app/src/main/kotlin/com/example/flutter_sqlite_auth_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_sqlite_auth_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile_app/lib/Components/colors.dart
+++
@@ -1,9 +0,0 @@
-
-
-import 'package:flutter/material.dart';
-
-//Our default project colors
-
-const primaryColor = Color.fromARGB(255, 0, 135, 218);
-const backgroundColor = Color(0xFFcfe0fa);
-const secondaryColor = Color(0xFFFFA2A2);
