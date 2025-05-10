01dd258ede96a72e0c7def483f7b38a3bc549fc0
Prithak Rai <prithak.khamtu@gmail.com>
2025-05-11 01:55:48 +0545

    Delete facerecognition_api directory
    Switched from Django to MQTT for communication between mobile app and the raspberry pi

--- facerecognition_api/recognition/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from recognition import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('', views.render_recognition_page, name='recognize_face_page'),  # Render the page with webcam feed
-    # Route to handle face recognition
-    path('recognize/', views.recognize_face, name='recognize_face'),
-]
